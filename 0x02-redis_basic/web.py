48319877c4db692eb0749fbf4f60e0987c69090e
bikriira <iradufashabikri@gmail.com>
2024-09-28 22:42:58 +0200

    [Achievement] implement real caching with real api

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# ~~~~~ TO SEE DIFERENCE IN EXECUTION TIME UNCOMMENT all ~~~~~#
+# from functools import wraps
+# import time
+import requests
+import redis
+
+re = redis.Redis()
+
+
+# def timer_decorator(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         result = func(*args, **kwargs)
+#         end_time = time.time()
+#         exec_time = end_time - start_time
+#         print(f"Execution time of {func.__name__}: {exec_time:.4f} seconds")
+#         return result
+#     return wrapper
+
+
+def cacher(f):
+    """Decorator that caches the result of a function.
+
+    Args:
+        f: The function to be cached.
+
+    Returns:
+        A wrapper function that implements caching.
+    """
+    def wrapper(*args):
+        url = args[0]
+        re.incr(f"count:{url}")
+        page_content = re.get(f"text:{url}")
+        if not page_content:
+            page_content = f(url)
+            re.setex(f"text:{url}", 100, page_content)
+        return page_content
+    return wrapper
+
+
+# @timer_decorator
+@cacher
+def get_page(url: str) -> str:
+    """Fetches the HTML content of a given URL.
+
+    Args:
+        url: The URL to fetch content from.
+
+    Returns:
+        The HTML content of the URL as a string.
+    """
+    response = requests.get(url)
+    return response.text
+
+
+# Example usage
+if __name__ == "__main__":
+    test_url = "http://slowwly.robertomurray.co.uk"
+    print(get_page(test_url)[:100])  # Print first 100 characters
+    print(f"Access count: {re.get(f'count:{test_url}').decode('utf-8')}")
