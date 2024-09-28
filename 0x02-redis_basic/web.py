bf286cef6a7871e76cb998adc9b4cb932cc9d1ff
bikriira <iradufashabikri@gmail.com>
2024-09-29 00:00:06 +0200

    [Try Fix]

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -41,9 +41,9 @@ def cacher(f: Callable) -> Callable:
     def wrapper(*args: list) -> str:
         """Wrapper function to cache the output of the decorated function."""
         url = args[0]
-        redis_client.incr(f"count:{url}")
         page_content = redis_client.get(f"text:{url}")
         if not page_content:
+            redis_client.incr(f"count:{url}")
             page_content = f(url)
             redis_client.setex(f"text:{url}", timedelta(seconds=10), page_content)
         else:
